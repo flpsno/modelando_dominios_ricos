32580485e7ea377b9af52de89677b8b89db83134
Felipe Santos <flpsno@hotmail.com>
2019-04-19 06:29:39 -0300

    adicionado os arquivos novamente

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Felipe", "Santos", "35700286823", "flpsno@hotmail.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
