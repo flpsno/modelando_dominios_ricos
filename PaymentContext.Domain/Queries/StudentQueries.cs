6f2422be3bc585aeeca76492c6ba14cb6612d59f
Felipe Santos <flpsno@hotmail.com>
2019-05-01 08:27:46 -0300

    adicionado teste para queries.

--- PaymentContext.Domain/Queries/StudentQueries.cs
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -4,7 +4,7 @@ using PaymentContext.Domain.Entities;
 
 namespace PaymentContext.Domain.Queries
 {
-    public static class StudeQueries
+    public static class StudentQueries
     {
         public static Expression<Func<Student, bool>> GetStudentInfo(string document)
         {
